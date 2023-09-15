19fbbe6ee6ee1ff3df3bafbb23ef819ce5bcd48e
William Nyongesa <marakawilliam@gmail.com>
2023-09-15 21:28:10 +0300

    completed assignment

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,3 +1,6 @@
+def get_names(spicy_foods):
+    return [food["name"] for food in spicy_foods]
+
 spicy_foods = [
     {
         "name": "Green Curry",
@@ -13,26 +16,180 @@ spicy_foods = [
         "name": "Mapo Tofu",
         "cuisine": "Sichuan",
         "heat_level": 6,
-    },
+    }
 ]
 
-def get_names(spicy_foods):
-    pass
+names = get_names(spicy_foods)
+print(names)  
+
+
+
+
+
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
+
+spicy_foods = [
+    {
+        "name": "Green Curry",
+        "cuisine": "Thai",
+        "heat_level": 9,
+    },
+    {
+        "name": "Buffalo Wings",
+        "cuisine": "American",
+        "heat_level": 3,
+    },
+    {
+        "name": "Mapo Tofu",
+        "cuisine": "Sichuan",
+        "heat_level": 6,
+    }
+]
+
+spiciest = get_spiciest_foods(spicy_foods)
+print(spiciest)
+
+    
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        heat_level = "🌶" * food["heat_level"]
+        print(f'{food["name"]} ({food["cuisine"]}) | Heat Level: {heat_level}')
 
-def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+spicy_foods = [
+    {
+        "name": "Green Curry",
+        "cuisine": "Thai",
+        "heat_level": 9,
+    },
+    {
+        "name": "Buffalo Wings",
+        "cuisine": "American",
+        "heat_level": 3,
+    },
+    {
+        "name": "Mapo Tofu",
+        "cuisine": "Sichuan",
+        "heat_level": 6,
+    }
+]
+
+print_spicy_foods(spicy_foods)
+
+
+
+
+
+
+
+
+
+    
+def get_spicy_food_by_cuisine(spicy_foods, target_cuisine):
+    for food in spicy_foods:
+        if food["cuisine"] == target_cuisine:
+            return food
+    return None  
+
+spicy_foods = [
+    {
+        "name": "Green Curry",
+        "cuisine": "Thai",
+        "heat_level": 9,
+    },
+    {
+        "name": "Buffalo Wings",
+        "cuisine": "American",
+        "heat_level": 3,
+    },
+    {
+        "name": "Mapo Tofu",
+        "cuisine": "Sichuan",
+        "heat_level": 6,
+    }
+]
+
+
+american_spicy_food = get_spicy_food_by_cuisine(spicy_foods, "American")
+print(american_spicy_food)  
+
+
+
+
+
+
+
+
+
+
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = get_spiciest_foods(spicy_foods)  
 
-def get_average_heat_level(spicy_foods):
-    pass
+    for food in spiciest_foods:
+        heat_level = "🌶" * food["heat_level"]  
+        print(f'{food["name"]} ({food["cuisine"]}) | Heat Level: {heat_level}')
 
-def create_spicy_food(spicy_foods, spicy_food):
-    pass
+spicy_foods = [
+    {
+        "name": "Green Curry",
+        "cuisine": "Thai",
+        "heat_level": 9,
+    },
+    {
+        "name": "Buffalo Wings",
+        "cuisine": "American",
+        "heat_level": 3,
+    },
+    {
+        "name": "Mapo Tofu",
+        "cuisine": "Sichuan",
+        "heat_level": 6,
+    }
+]
+
+print_spiciest_foods(spicy_foods)
+
+
+
+
+
+
+
+
+def average_heat_level(spicy_foods):
+    total_heat = sum(food["heat_level"] for food in spicy_foods)  
+    num_foods = len(spicy_foods)  
+    if num_foods == 0:
+        return 0  
+    else:
+        average = total_heat / num_foods 
+        return round(average)  
+
+spicy_foods = [
+    {
+        "name": "Green Curry",
+        "cuisine": "Thai",
+        "heat_level": 9,
+    },
+    {
+        "name": "Buffalo Wings",
+        "cuisine": "American",
+        "heat_level": 3,
+    },
+    {
+        "name": "Mapo Tofu",
+        "cuisine": "Sichuan",
+        "heat_level": 6,
+    }
+]
+
+average_heat = average_heat_level(spicy_foods)
+print(average_heat)  
+
+
+
+# def create_spicy_food(spicy_foods, spicy_food):
+#     pass
